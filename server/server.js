a05d9cad333f0b27713cbb546b52c3b1c28e62e9
Kevin Mangal <kmangal818@gmail.com>
2018-09-03 16:58:14 -0400

    Fixed port env var

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const path = require('path');
 const express = require('express');
 
 const publicPath = path.join(__dirname, '../public');
-const port = process.env.port || 8000;
+const port = process.env.PORT || 8000;
 var app = express();
 
 app.use(express.static(publicPath));
