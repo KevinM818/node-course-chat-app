9d2daea18539bbbdb1ce2dc8e956179e3e683703
Kevin Mangal <kmangal818@gmail.com>
2018-09-03 19:06:11 -0400

    add new message and create message events

--- server/server.js
+++ server/server.js
@@ -14,9 +14,19 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
 	console.log('New user connected');
 
+	socket.emit('newMessage', {
+		from: 'Kevin',
+		text: 'Hey. What is going on.',
+		createAt: 123 	
+	});
+
+	socket.on('createMessage', (message) => {
+		console.log('createMessage', message);
+	});
+
 	socket.on('disconnect', () => {
 		console.log('User was disconnected');
-	})
+	});
 });
 
 server.listen(port, () => console.log(`Server 1s up on port ${port}`));
